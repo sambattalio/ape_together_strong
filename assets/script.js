4142b39a64f6f75dfd7a72c2a1a2215844d36ff6
eamonlm <elopez7@nd.edu>
2021-03-18 17:56:46 -0400

    added capability to display graphs

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// Script to Display change in weights for an exercise over time
+
+var weight = [15, 25, 35];
+var dates = ['03/01', '03/08', '03/15'];
+
+var ctx = document.getElementById('exerciseChangeChart');
+
+var exerciseChangeChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+	labels: dates,
+	datasets: [{
+	    label: 'Exercise Weights over Time',
+	    data: weight,
+	    backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+	    fill: false,
+	    lineTension: 0
+	}]
+    },
+    
+    options: {
+	maintainAspectRatio: false
+	//beginAtZero: true
+    }
+});
