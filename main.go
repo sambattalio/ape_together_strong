33f8f43ed6328f95d5c2b79828adb56166586ebf
sambattalio <sambattalio99@gmail.com>
2021-03-18 17:32:41 -0400

    working more

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
     "fmt"
     "net/http"
+    "encoding/json"
 
     "github.com/gorilla/mux"
 )
+
+type Workout struct {
+    Exercise string `json:"exercise"`
+    Weight   string `json:"weight"`
+}
+
+// i don't like this
+var workouts []Workout
+
 
 func main() {
     r := buildRouter()
@@ -17,7 +27,8 @@ func buildRouter() *mux.Router {
 
     // set up RESTful endpoints
     r.HandleFunc("/hello", handler).Methods("GET")
-    r.HandleFunc("/workout", workoutPut).Methods("PUT")
+    r.HandleFunc("/workout", createWorkoutHandler).Methods("POST")
+    r.HandleFunc("/workout", getWorkoutHandler).Methods("GET")
 
     // load up static files
     staticFileDir := http.Dir("./assets/")
@@ -32,6 +43,37 @@ func handler (w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello world")
 }
 
-func workoutPut(w http.ResponseWriter, r *http.Request) {
-    // TODO
+
+func getWorkoutHandler(w http.ResponseWriter, r *http.Request) {
+    workoutListBytes, err := json.Marshal(workouts)
+
+    if err != nil {
+        fmt.Println(fmt.Errorf("Error: %v", err))
+	w.WriteHeader(http.StatusInternalServerError)
+	return
+    }
+
+    w.Write(workoutListBytes)
+}
+
+func createWorkoutHandler(w http.ResponseWriter, r *http.Request) {
+    workout := Workout{}
+
+    err := r.ParseForm()
+
+    if err != nil {
+        fmt.Println(fmt.Errorf("Error: %v", err))
+	w.WriteHeader(http.StatusInternalServerError)
+	return
+    }
+
+    workout.Exercise = r.Form.Get("exercise")
+    workout.Weight   = r.Form.Get("weight")
+
+    fmt.Println(workout.Exercise)
+
+    workouts = append(workouts, workout)
+
+
+    http.Redirect(w, r, "/assets/", http.StatusFound)
 }
